16bee5b82d0c0fc98b447ced76d8497b70127ee5
xxasimovxx <asimovpl@wp.pl>
2024-01-06 23:40:26 +0100

    tic-tac-toe

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container">
+
+      );
+
+      </div>
+    </>
+  )
+}
+
+function makeTiles(){
+  return(
+    for(let i = 0; i < 3; i++){
+      
+    }
+  );
+}
+
