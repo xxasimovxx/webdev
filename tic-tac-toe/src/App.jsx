18a2b432ad4b6ee93647e4a3740507c81f923170
xxasimovxx <asimovpl@wp.pl>
2024-01-13 01:06:29 +0100

    tic-tac-toe

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -6,46 +6,65 @@ export default function App() {
   const [tiles, setTiles] = useState([]);
   const [isO, setisO] = useState(true);
 
-function makeTiles(){
-      
-  for(let i = 1; i <=9; i++){
-    // console.log(i)
-    useEffect(()=>{
+  const winningCombinations=[
+    {combination: [1,2,3]},
+    {combination: [4,5,6]},
+    {combination: [7,8,9]},
+    {combination: [1,4,7]},
+    {combination: [2,5,8]},
+    {combination: [3,6,9]},
+    {combination: [1,5,9]},
+    {combination: [3,5,7]},
+
+]
+
+  console.log(winningCombinations[0])
+  
+
+  function makeTiles(){
+        
+    for(let i = 1; i <=9; i++){
+      // console.log(i)
+      useEffect(()=>{
+
+      setTiles(currentTiles =>{
+        return[
+
+          ...currentTiles, 
+          {id: i, content: "blank", isClicked: false}
+        ]
+      })
+      },[])
+
+    }}
+
+  function changeTile(isO, id){
 
     setTiles(currentTiles =>{
-      return[
+      return currentTiles.map(tile => {
+        if ((tile.id == id) && (!tile.isClicked)){
+          if(isO){
 
-        ...currentTiles, 
-        {id: i, content: "blank", isClicked: false}
-      ]
-    })
-    },[])
+            setisO(!isO)
 
-  }}
-
-function changeTile(isO, id){
-
-  setTiles(currentTiles =>{
-    return currentTiles.map(tile => {
-      if ((tile.id == id) && (!tile.isClicked)){
-        if(isO){
-
-          setisO(!isO)
-
-          return {id:id, content: "circle", isClicked: true}
-        }else{
+            return {id:id, content: "circle", isClicked: true}
+          }else{
+            
+            setisO(!isO)
+            return {id:id, content: "cross", isClicked: true}
+          }
           
-          setisO(!isO)
-          return {id:id, content: "cross", isClicked: true}
         }
-        
-      }
 
-      return tile
+        return tile
+      })
     })
-  })
 
-}
+  }
+
+  function checkWinner(){
+
+  }
 
   makeTiles();
 
