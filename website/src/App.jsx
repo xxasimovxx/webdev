7a98860a04c119e2c3bc53ed503e94e24a912837
xxasimovxx <asimovpl@wp.pl>
2024-01-24 00:31:33 +0100

    website

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAtom } from '@fortawesome/free-solid-svg-icons'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+
+       <div className="outer">
+
+          <div className="subject"><FontAwesomeIcon icon={faAtom} size = "6x"/>
+
+            <div className="subject-description">
+
+              <h1>Fizyka</h1>
+              <h3>Zadania maturalne  2010 - 2023</h3>
+            </div>
+          </div>
+        </div> 
     </>
   )
 }
