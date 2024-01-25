d28657029635cfc9de468be661685514aa266f6d
xxasimovxx <asimovpl@wp.pl>
2024-01-25 01:17:02 +0100

    website

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,27 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import  Standard_Tile from "./components/standard_tile.jsx"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAtom } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
+export default function App() {
 
   return (
     <>
 
        <div className="outer">
+        {Standard_Tile({"Fizyka", "Z"})}
 
-          <div className="subject"><FontAwesomeIcon icon={faAtom} size = "6x"/>
-
-            <div className="subject-description">
-
-              <h1>Fizyka</h1>
-              <h3>Zadania maturalne  2010 - 2023</h3>
-            </div>
-          </div>
         </div> 
     </>
   )
 }
 
-export default App
---
+++ website/src/components/standard_tile.jsx
@@ -0,0 +1,16 @@
+
+import '../App.css'
+
+export default function Standard_Tile( name, description){
+    return
+
+        <div className="tile">
+
+            <div className="tile-description">
+
+              <h1>{name}</h1>
+              <h3>{description}</h3>
+        </div>
+        </div>
+        
+}
