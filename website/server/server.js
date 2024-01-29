8e71b15e06658036ea032845a49bf0f9127c4e1e
xxasimovxx <asimovpl@wp.pl>
2024-01-29 23:56:43 +0100

    website

--- website/server/server.js
+++ website/server/server.js
@@ -1,11 +1,28 @@
 const express = require("express")
-const app = express()
+const React = require("react")
+const ReactDOMServer = require("react-dom/server");
 const path = require("path")
+const fs = require("fs")
 
-app.use(express.static(path.join(__dirname, "frontend")))
+const app = express()
 
-app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname, "frontend","index.html"))
+const App  =  require("./frontend/src/App.jsx")
+
+app.use(express.static(path.join(__dirname, "frontend/src")))
+
+app.use("^/$", (req,res,next)=>{
+    fs.readFile(path.resolve("./frontend/src/App.jsx"), "utf-8", (err, data) =>{
+        if(err){
+            console.log(err)
+            return res.status(500)
+        }
+        return res.send(
+            data.replace(
+                `<div id="root"><div/>`,
+                `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+            )
+        )
+    })
 })
 
 app.listen(3000, ()=>{
