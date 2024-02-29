087b4b01a0461609d8d4e9f5439e144d5b63c599
xxasimovxx <asimovpl@wp.pl>
2024-02-29 23:36:48 +0100

    final

--- website/site/src/Show_Subject.jsx
+++ website/site/src/Show_Subject.jsx
@@ -1,25 +1,60 @@
+import { useState } from "react";
+
 const importAll = (require) =>
   require.keys().reduce((acc, next) => {
     acc[next.replace("./", "")] = require(next);
     return acc;
   }, {});
 
-const images = Object.keys(
-  importAll(require.context("./images", false, /\.png/)))
-
-
-
 export default function Show_Subject({ name }) {
+  const [blurred, setBlurred] = useState(true);
+
+  const handleCLick = () => {
+    setBlurred(!blurred);
+  };
+
+  const images = Object.keys(
+    importAll(require.context("./images", true, /\.png/)),
+  );
+
   return (
     <>
       <div className="outer">
-        {
-          images.map(key => {
-            return (<div key = {key}>
-              <img key={key} src={require("./images/" + key)} alt="asd" />,
+        {images.map((key) => {
+          if (key.split("/").includes(name)) {
+            return (
+              <div key={key + "out"} className="image-outer">
+                <img className="image-task" key={key} src={require("./images/" + key)} alt="asd" />
+                <div className="answer-container">
+                  <div className="image-outer">
+                    <img
+                      key={key + "odpowiedz"}
+                      src={require(
+                        "./odpowiedz/" + key.split(".")[0] + "-odpowiedz.png",
+                      )}
+                      style={{
+                        filter: blurred ? "blur(5px)" : "",
+                        zIndex: "1",
+                      }}
+                      onClick={handleCLick}
+                      alt="asd"
+                    />
+                    <h1
+                      style={{
+                        position: "absolute",
+                        visibility: blurred ? "" : "hidden",
+                        zIndex: "2",
+                      }}
+                    >
+                      Show Answer
+                    </h1>
+                  </div>
+                </div>
+                ,
               </div>
-          )
-          })}
+            );
+          }
+        })}
       </div>
     </>
   );
