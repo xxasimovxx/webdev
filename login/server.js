ccbb508381051d5efa26d5929e59aec0bf962ec7
xxasimovxx <asimovpl@wp.pl>
2024-03-05 00:43:12 +0100

    login

--- login/server.js
+++ login/server.js
@@ -2,9 +2,10 @@ const  express = require( "express")
 const path = require("path")
 const app = express()
 
+app.use(express.static(path.join(__dirname, '/public')));
 app.get("/", (_req, res) => {
 
-  res.sendFile(path.join(__dirname + "/index.html"))
+  res.sendFile(path.join(__dirname + "/public/index.html"))
 
 
 })
